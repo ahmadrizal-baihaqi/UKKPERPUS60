5526faf502beeb0a135b40614c66bbcd32ba5241
Rizal <ahmadrizalbaihaqi2@gmail.com>
2026-04-13 07:54:35 +0700

    push 60

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role', // Untuk mengisi role saat membuat user
+        'role', 
     ];
 
     /**
